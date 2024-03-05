44f445a532deb9d17b8e3245db8200b0fa38c9c7
KayStorm <93082092+KayStorm@users.noreply.github.com>
2024-03-05 09:29:06 -0500

    readability improvements

--- 100sametreeSOLVED.py
+++ 100sametreeSOLVED.py
@@ -6,21 +6,18 @@
 
 # Example 1:
 
-
 # Input: p = [1,2,3], q = [1,2,3]
 # Output: true
 # Example 2:
-
 
 # Input: p = [1,2], q = [1,null,2]
 # Output: false
 # Example 3:
 
-
 # Input: p = [1,2,1], q = [1,1,2]
 # Output: false
  
-
+ 
 # Constraints:
 
 # The number of nodes in both trees is in the range [0, 100].
