ac059c25e9f36fc59a8c8a044b65f0d723d8afa1
KayStorm <93082092+KayStorm@users.noreply.github.com>
2024-03-12 17:49:12 -0400

    solution complete

--- 2769findmaxnumberSOLVED.py
+++ 2769findmaxnumberSOLVED.py
@@ -33,9 +33,4 @@
 
 class Solution(object):
     def theMaximumAchievableX(self, num, t):
-        """
-        :type num: int
-        :type t: int
-        :rtype: int
-        """
-       
+        return num + (t+t)
