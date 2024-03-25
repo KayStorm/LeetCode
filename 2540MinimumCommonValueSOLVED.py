35e20ad0969e4ec1f3e3dd35adb08c2c458fbf43
KayStorm <93082092+KayStorm@users.noreply.github.com>
2024-03-25 11:22:20 -0400

    solved problem

--- 2540MinimumCommonValueSOLVED.py
+++ 2540MinimumCommonValueSOLVED.py
@@ -20,6 +20,7 @@
 # Both nums1 and nums2 are sorted in non-decreasing order.
 
 
-class Solution(object):
-    def getCommon(self, nums1, nums2):
-      
+class Solution:
+    def getCommon(self, nums1: List[int], nums2: List[int]) -> int:
+        inter=set(nums1).intersection(set(nums2))
+        return min(inter) if len(inter)>0  else -1 
